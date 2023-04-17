2f601a0c8fa2c09ec5d8986723e5ddd829ead3ca
Emmanuel Baimba <emmanuelleosamuel203@gmail.com>
2023-04-17 11:49:17 +0000

    This is the first push for the frontend of the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-   
+     <h1>Testing the main app for this</h1>
     </div>
   );
 }
